a06f970d8fafbbfdb3db2b75af4c937adfb16b84
DatPika <levitind@gmail.com>
2024-11-29 15:56:19 -0500

    GET /athlete filters validation and throws new custom exception

--- app/src/Exceptions/HttpUnsupportedMediaTypeException.php
+++ app/src/Exceptions/HttpUnsupportedMediaTypeException.php
@@ -2,9 +2,7 @@
 
 namespace App\Exceptions;
 
-use Psr\Http\Message\ServerRequestInterface;
 use Slim\Exception\HttpSpecializedException;
-use Throwable;
 
 class HttpUnsupportedMediaTypeException extends HttpSpecializedException
 {
@@ -12,19 +10,4 @@ class HttpUnsupportedMediaTypeException extends HttpSpecializedException
     protected $message = "Request header contains unsupported media types.";
     protected string $title = "Unsupported Media Type";
     protected string $description = "Currently, the only supported media type is 'application/json'.";
-
-
-    // /**
-    //  * @param ServerRequestInterface $request
-    //  * @param string|null            $message
-    //  * @param Throwable|null         $previous
-    //  */
-    // public function __construct(ServerRequestInterface $request, ?string $message = null, ?Throwable $previous = null)
-    // {
-    //     if ($message !== null) {
-    //         $this->message = $message;
-    //     }
-
-    //     parent::__construct($request, $this->message, $this->code, $previous);
-    // }
 }
